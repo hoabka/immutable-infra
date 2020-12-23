6a66a5c1cbe60f5a314384211bacc08ddc191eee
hoabka <nguyenhuuhoa010292@gmail.com>
2020-12-23 22:11:30 +0700

    Update packer-build.sh

--- vm-based/infra/packer/scripts/packer-build.sh
+++ vm-based/infra/packer/scripts/packer-build.sh
@@ -6,24 +6,13 @@ function build_image() {
     export role_name=$1
     echo ${role_name}
     #Inspect script
-    packer \
-    inspect \
-      packer.json \
-    ;
+    packer inspect packer.json
 
     #Validate packer script
-    packer \
-    validate \
-      -var-file "variables.json" \
-      packer.json \
-    ;
+    packer validate -var-file variables.json packer.json
+    
     #Packaging image
-    packer \
-    build \
-      -var-file "variables.json" \
-      packer.json \
-      -debug       \
-    ;
+    packer build -var-file variables.json packer.json -debug
 }
 echo $1
 build_image $1
