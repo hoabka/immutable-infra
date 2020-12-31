986ff421c8015710de39c0a7e851eae9d2f33e09
hoabka <hoabka@gmail.com>
2020-12-31 17:27:57 +0700

    2nd commit

--- vm-based/infra/aws/scripts/init-deployment.bash
+++ vm-based/infra/aws/scripts/init-deployment.bash
@@ -5,5 +5,5 @@ date
 ansible-playbook \
   --inventory-file ansible-hosts.yml \
   --tags "deployment" \
-    "${BASE_DIR_PATH}/ansible/initial-deployment.yml" \
+    "../../ansible/initial-deployment.yml" \
     ;
