5453786c36d398e118b33d5948d5d24b79110e5f
hoabka <hoabka@gmail.com>
2021-01-04 10:15:27 +0700

    Edit bastion publicip

--- vm-based/infra/aws/local-files.tf
+++ vm-based/infra/aws/local-files.tf
@@ -1,7 +1,7 @@
 resource "local_file" "ansible-hosts" {
   filename = "${path.cwd}/ansible-hosts.yml"
   content = templatefile("${path.cwd}/templates/ansible-hosts.tpl", {
-    bastion_public_ip = aws_instance.bastion.public_ip
+    bastion_public_ip = aws_eip.bastion.public_ip
     web_private_ips   = join(",", aws_instance.web.*.private_ip)
     app_private_ips   = join(",", aws_instance.app.*.private_ip)
     tf_remote_user    = "ubuntu"
@@ -11,7 +11,7 @@ resource "local_file" "ansible-hosts" {
 resource "local_file" "ssh-conf" {
   filename = "${path.cwd}/ssh.conf"
   content  = templatefile("${path.cwd}/templates/ssh.conf.tpl", {
-    bastion_public_ip            = aws_instance.bastion.public_ip
+    bastion_public_ip            = aws_eip.bastion.public_ip
     ssh_private_key_path_bastion = "${local.private_keyname_path}/bastion.pem"
     web_private_ips              = join(",", aws_instance.web.*.private_ip)
     ssh_private_key_path_web     = "${local.private_keyname_path}/web.pem"
